5610e78e0144db75095dbb8ab329fac1cc838bf9
Ignat Georgiev <ignat.m.georgiev@gmail.com>
2024-04-26 18:02:42 -0400

    finalize double pendulum

--- pathological/double_pendulum.py
+++ pathological/double_pendulum.py
@@ -1,35 +1,25 @@
+import hydra
 import torch
-from torch.distributions.normal import Normal
-from torch.autograd import Variable
 import matplotlib.pyplot as plt
 import seaborn as sns
 from tqdm import tqdm
-from forl.models.mlp import mlp, SimNorm
-from torch.optim import Adam
-import torch.nn as nn
-from forl.models.actor import ActorDeterministicMLP, ActorStochasticMLP
 from forl.utils import torch_utils as tu
 
 from dflex.envs.double_pendulum import DoublePendulumEnv
-from dflex.envs.cartpole_swing_up import CartPoleSwingUpEnv
-from dflex.envs.hopper import HopperEnv
 from forl.utils.common import seeding
-import hydra
 from utils import dflex_jacobian
 from torch.autograd.functional import jacobian
 
-
-import matplotlib.pyplot as plt
-import seaborn as sns
+# TDMPC libraries
+from tdmpc2 import TDMPC2
+from common.parser import parse_cfg
+from common.math import two_hot_inv
+from common.init import weight_init
 
 sns.set()
 
 from IPython.core import ultratb
 import sys
-
-from tdmpc2 import TDMPC2
-from common.parser import parse_cfg
-from common.math import two_hot_inv
 
 # For debugging
 sys.excepthook = ultratb.FormattedTB(
@@ -39,44 +29,16 @@ sys.excepthook = ultratb.FormattedTB(
 
 @hydra.main(config_name="config", config_path=".")
 def main(cfg: dict):
-    # We need:
-    # 1) Actor gradients from simulation
-    # 2) Actor gradients from model
-    #   2.1) We need data from the simulation
-    #   2.2) We need to train the model
-    # 3) Compute gradients, compare their
-    #   - gradient norms
-    #   - eigenvalues
-    #   - variance
-    #   - Signal to noise ratio
-    device = "cuda"
-    seeding(0, True)
     env = DoublePendulumEnv(
         render=False,
         num_envs=1,
         episode_length=240,
         no_grad=False,
         stochastic_init=False,
+        device=cfg.device,
     )
 
-    actor = ActorStochasticMLP(
-        env.num_obs, env.num_acts, [400, 200, 100], torch.nn.Mish
-    ).to(device)
-    # cartpole
-    # chkpt = torch.load(
-    #     "/storage/home/hcoda1/7/igeorgiev3/git/FoWM/scripts/outputs/2024-04-21/19-37-34/logs/best_policy.pt",
-    #     map_location=device,
-    # )
-    # doublependulum
-    chkpt = torch.load(
-        "/storage/home/hcoda1/7/igeorgiev3/git/FoWM/scripts/outputs/2024-04-23/16-14-35/logs/best_policy.pt",
-        map_location=device,
-    )
-    actor.load_state_dict(chkpt["actor"])
-    og_state_dict = actor.state_dict()
-    obs_rms = chkpt["obs_rms"].to(device)
-    opt = Adam(actor.parameters(), lr=1e-3)
-    H = 100
+    H = 50
 
     cfg = parse_cfg(cfg)
     cfg.obs_shape = {"state": env.obs_space.shape}
@@ -86,14 +48,17 @@ def main(cfg: dict):
 
     # pre-trained TDMPC models; H: model_path
     models = {
-        4: "/storage/home/hcoda1/7/igeorgiev3/git/FoWM/wmlab/logs/dflex-doublependulum/000/pretrain_baseline/models/final.pt",
+        4: "/storage/home/hcoda1/7/igeorgiev3/git/FoWM/wmlab/logs/dflex-doublependulum/0/pretrain_baseline/models/final.pt",
         # 8: "/storage/home/hcoda1/7/igeorgiev3/git/FoWM/wmlab/logs/dflex-cartpole/1/pretrain_baseline/models/final.pt",
         # 16: "/storage/home/hcoda1/7/igeorgiev3/git/FoWM/wmlab/logs/dflex-cartpole/2/pretrain_baseline/models/final.pt",
         # 32: "/storage/home/hcoda1/7/igeorgiev3/git/FoWM/wmlab/logs/dflex-cartpole/3/pretrain_baseline/models/final.pt",
     }
+    tdmpc.load(models[4])
+    seeding(0)
+    tdmpc.model._pi.apply(weight_init)  # reset actor weight
 
     # save actions from first rollout to account for the fact that the model is stochastic
-    actions = torch.empty((H, cfg.action_dim)).to(device)
+    actions = torch.empty((H, cfg.action_dim)).to(cfg.device)
 
     f, ax = plt.subplots(2, 2, figsize=(8, 6))
     # prediction error, per-step dynamics jacobian norm, per-step dynamics jacobian max eigenvalue, whole policy gradient norm
@@ -101,46 +66,46 @@ def main(cfg: dict):
 
     # now also compute jacobians
     print("Computing jacobians for dflex")
-    jac_norms = torch.empty((H,)).to(device)
-    jac_eig = torch.empty((H, 4)).to(device)
-    losses = torch.empty((H,)).to(device)
+    jac_norms = torch.empty((H,)).to(cfg.device)
+    jac_eig = torch.empty((H, 4)).to(cfg.device)
+    losses = torch.empty((H,)).to(cfg.device)
 
-    torch.manual_seed(0)
-    obs = env.reset()
-    obs = env.initialize_trajectory()
-    # rewards = torch.tensor([0.0]).to(device)
+    seeding(0)
+    obs = env.reset(grads=True)
     for h in tqdm(range(H)):
-        # opt.zero_grad()
-        obs = obs_rms.normalize(obs)
-        act = actor(obs, deterministic=True)
-        actions[h] = act.detach().clone()
-        jac = dflex_jacobian(env, obs, act)
+        z = tdmpc.model.encode(obs, None)
+        a = tdmpc.model.pi(z.detach(), None)[1]
+        # obs = obs_rms.normalize(obs)
+        # act = actor(obs, deterministic=True)
+        actions[h] = a.detach().clone()
+        jac = dflex_jacobian(env, obs, a)
         jac = jac[..., :4, :4]  # need square matrix # TODO better code
         jac = torch.nan_to_num(jac)
         jac_norms[h] = jac.norm()
-        # breakpoint()
         jac_eig[h] = torch.real(torch.linalg.eigvals(jac))
-        obs, rew, done, info = env.step(act)
+        obs, rew, done, info = env.step(a)
         losses[h] = rew + losses[h - 1] if h > 0 else rew
-        # rewards += rew
-        # rewards.sum().backward(retain_graph=True)
-        # policy_norms[h] = tu.grad_norm(actor.parameters())
 
     print("Computing policy grad norms for dflex")
-    policy_norms = torch.zeros((H,)).to(device)
+    policy_norms = torch.zeros((H,)).to(cfg.device)
     for h in tqdm(range(1, H + 1)):
         obs = env.reset(grads=True)
-        opt.zero_grad()
-        torch.manual_seed(0)  # guarantee same policy actions
-        rew = torch.zeros(env.num_envs).to(device)
+        tdmpc.pi_optim.zero_grad()
+        seeding(0)  # guarantee same policy actions
+        rew = torch.zeros(env.num_envs).to(cfg.device)
         for t in range(h):
-            obs = obs_rms.normalize(obs)
-            a = actor(obs.detach())
+            # obs = obs_rms.normalize(obs)
+            # a = actor(obs.detach())
+            z = tdmpc.model.encode(obs, None)
+            a = tdmpc.model.pi(z.detach(), None)[1]
             obs, reward, done, info = env.step(a)
+            # do reward transformation to be comparable with tdmpc
+            reward /= 21554.2305
+            reward += 1.0
             rew += reward
 
         rew.mean().backward()
-        policy_norms[h - 1] = tu.grad_norm(actor.parameters())
+        policy_norms[h - 1] = tu.grad_norm(tdmpc.model._pi.parameters())
 
     ax[1].plot(jac_norms.detach().cpu(), label="GT")
     ax[2].plot(jac_eig.max(dim=1).values.detach().cpu(), label="GT")
@@ -151,20 +116,20 @@ def main(cfg: dict):
     for hh, filepath in models.items():
         print(f"Loading TDMPC model with H={hh}")
         tdmpc.load(filepath)
+        seeding(0)
+        tdmpc.model._pi.apply(weight_init)  # reset actor weight
 
         # now compute just jacobians since those don't depend on deltas
         print("Computing jacobians for TDMPC")
-        jac_norms = torch.empty((H,)).to(device)
-        jac_eig = torch.empty((H, cfg.latent_dim)).to(device)
-        td_losses = torch.empty((H,)).to(device)
+        jac_norms = torch.empty((H,)).to(cfg.device)
+        jac_eig = torch.empty((H, cfg.latent_dim)).to(cfg.device)
+        td_losses = torch.empty((H,)).to(cfg.device)
         obs = env.reset(grads=True)
         obs = env.initialize_trajectory()
         z = tdmpc.model.encode(obs, None)
         for h in tqdm(range(H)):
-            # breakpoint()
             act = actions[h].unsqueeze(0)
             rew = two_hot_inv(tdmpc.model.reward(z, act, None), cfg)[0]
-            # print(rew)
             z = tdmpc.model.next(z, act, None)
             jac = jacobian(tdmpc.model.next, (z, act, torch.zeros((1,))))
             jac = jac[0].squeeze()
@@ -183,28 +148,22 @@ def main(cfg: dict):
         jj += 1
 
         print("Computing actor norms for TDMPC")
-        policy_norms = torch.zeros((H,)).to(device)
+        policy_norms = torch.zeros((H,)).to(cfg.device)
         for h in tqdm(range(1, H + 1)):
             obs = env.reset(grads=True)
             z = tdmpc.model.encode(obs, None)
             tdmpc.pi_optim.zero_grad()
-            # opt.zero_grad()
-            # actor.load_state_dict(og_state_dict)
-            torch.manual_seed(0)  # guarantee same policy actions
-            rew = torch.zeros(env.num_envs).to(device)
+            seeding(0)  # guarantee same policy actions
+            rew = torch.zeros(env.num_envs).to(cfg.device)
             for t in range(h):
-                # obs = obs_rms.normalize(obs)
-                # a = actor(obs.detach())
                 a = tdmpc.model.pi(z, None)[1]
                 r = two_hot_inv(tdmpc.model.reward(z, a, None), cfg)[0]
                 rew += r
-                # obs, reward, done, info = env.step(a)
                 z = tdmpc.model.next(z, a, None)
 
             rew.mean().backward()
             policy_norms[h - 1] = tu.grad_norm(tdmpc.model._pi.parameters())
 
-        # breakpoint()
         ax[3].plot(policy_norms.detach().cpu(), label=f"TD H={hh}")
 
     print("Saving figure")
--- pathological/double_pendulum_var.py
+++ pathological/double_pendulum_var.py
@@ -1,3 +1,4 @@
+import hydra
 import torch
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -5,21 +6,20 @@ from tqdm import tqdm
 
 from dflex.envs.double_pendulum import DoublePendulumEnv
 from forl.utils.common import seeding
-import hydra
 
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-sns.set()
-
-from IPython.core import ultratb
-import sys
-
+# TDMPC libraries
 from tdmpc2 import TDMPC2
+from tdmpc2_simple import TDMPC2 as TDMPC2_simple
+from tdmpc2_no_simnorm import TDMPC2 as TDMPC2_no_simnorm
 from common.parser import parse_cfg
 from common.math import two_hot_inv
 from common.init import weight_init
+
+sns.set()
+
+
+from IPython.core import ultratb
+import sys
 
 # For debugging
 sys.excepthook = ultratb.FormattedTB(
@@ -40,26 +40,34 @@ def main(cfg: dict):
         # start_state=[np.pi / 2, np.pi / 2, 0.0, 0.0],
         device=device,
     )
-    H = 10
-    samples = 10
+    H = 100
+    samples = 100
 
-    cfg = parse_cfg(cfg)
-    cfg.obs_shape = {"state": env.obs_space.shape}
-    cfg.action_dim = env.act_space.shape[0]
-    cfg.episode_length = env.episode_length
-    tdmpc = TDMPC2(cfg)
-
-    # pre-trained TDMPC models; H: model_path
-    models = {
-        4: "/storage/home/hcoda1/7/igeorgiev3/git/FoWM/wmlab/logs/dflex-doublependulum/100/pretrain_baseline/models/final.pt",
-        8: "/storage/home/hcoda1/7/igeorgiev3/git/FoWM/wmlab/logs/dflex-doublependulum/101/pretrain_baseline/models/final.pt",
-        16: "/storage/home/hcoda1/7/igeorgiev3/git/FoWM/wmlab/logs/dflex-doublependulum/102/pretrain_baseline/models/final.pt",
-        32: "/storage/home/hcoda1/7/igeorgiev3/git/FoWM/wmlab/logs/dflex-doublependulum/103/pretrain_baseline/models/final.pt",
+    model_paths = {
+        "TDMPC": "/storage/home/hcoda1/7/igeorgiev3/git/FoWM/wmlab/logs/dflex-doublependulum/100/pretrain_baseline/models/final.pt",
+        "TDMPC_no_sn": "/storage/home/hcoda1/7/igeorgiev3/git/FoWM/wmlab/logs/dflex-doublependulum/200/pretrain_baseline/models/final.pt",
+        "TDMPC_simple": "/storage/home/hcoda1/7/igeorgiev3/git/FoWM/wmlab/logs/dflex-doublependulum/300/pretrain_tdmpc_simple/models/final.pt",
     }
 
-    tdmpc.load(models[4])
-    tdmpc.model._pi.apply(weight_init)  # reset actor weight
-    tdmpc.model.eval()
+    tdmpcs = {}
+    for key in model_paths:
+        print(f"Loading {key}")
+        cfg = parse_cfg(cfg)
+        cfg.obs_shape = {"state": env.obs_space.shape}
+        cfg.action_dim = env.act_space.shape[0]
+        cfg.episode_length = env.episode_length
+        clas = TDMPC2
+        if key == "TDMPC_no_sn":
+            clas = TDMPC2_no_simnorm
+        elif key == "TDMPC_simple":
+            clas = TDMPC2_simple
+        tdmpc = clas(cfg)
+
+        tdmpc.load(model_paths[key])
+        seeding(0)  # keep model weights the same
+        tdmpc.model._pi.apply(weight_init)  # reset actor weight
+        tdmpc.model.eval()
+        tdmpcs.update({key: tdmpc})
 
     f, ax = plt.subplots(1, 2, figsize=(8, 3))
     ax = ax.flatten()
@@ -99,36 +107,40 @@ def main(cfg: dict):
     # ax[0].fill_between(range(H), lower_bound, upper_bound, alpha=0.5)
     ax[1].plot(range(H), policy_snr, label="GT")
 
-    print("Computing policy variance for tdmpc")
-    seeding(0)
-    all_grads = []
-    for h in tqdm(range(1, H + 1)):
-        grads = []
-        for _ in range(samples):
-            obs = env.reset(grads=True)
-            z = tdmpc.model.encode(obs, None)
-            tdmpc.pi_optim.zero_grad()
-            rew = torch.zeros(env.num_envs).to(device)
-            for _ in range(h):
-                a = tdmpc.model.pi(z.detach(), None)[1]
-                r = two_hot_inv(tdmpc.model.reward(z, a, None), cfg)[0]
-                rew += r
-                z = tdmpc.model.next(z, a, None)
+    for key, tdmpc in tdmpcs.items():
+        print(f"Computing policy variance for {key}")
+        seeding(0)
+        all_grads = []
+        for h in tqdm(range(1, H + 1)):
+            grads = []
+            for _ in range(samples):
+                obs = env.reset(grads=True)
+                z = tdmpc.model.encode(obs, None)
+                tdmpc.pi_optim.zero_grad()
+                rew = torch.zeros(env.num_envs).to(device)
+                for _ in range(h):
+                    a = tdmpc.model.pi(z.detach(), None)[1]
+                    if key != "TDMPC_simple":
+                        r = two_hot_inv(tdmpc.model.reward(z, a, None), cfg)[0]
+                    else:
+                        r = tdmpc.model.reward(z, a, None)[0]
+                    rew += r
+                    z = tdmpc.model.next(z, a, None)
 
-            (-rew).mean().backward()
-            grads.append(get_grads(tdmpc.model._pi))
-        grads = torch.vstack(grads)
-        all_grads.append(grads)
+                (-rew).mean().backward()
+                grads.append(get_grads(tdmpc.model._pi))
+            grads = torch.vstack(grads)
+            all_grads.append(grads)
 
-    # Plotting!
-    grads = torch.stack(all_grads)  # H x samples x n_params
-    save_data["tdmpc_grads"] = grads
-    variance = grads.var(dim=1).mean(dim=1)
-    variance_std = grads.var(dim=1).std(dim=1)
-    policy_snr = (grads.mean(dim=1) ** 2 / (grads.var(dim=1) + 1e-9)).mean(dim=1)
-    ax[0].plot(range(H), variance, label="TDMPC")
-    # ax[0].fill_between(range(H), lower_bound, upper_bound, alpha=0.5)
-    ax[1].plot(range(H), policy_snr, label="TDMPC")
+        # Plotting!
+        grads = torch.stack(all_grads)  # H x samples x n_params
+        save_data[f"{key}_grads"] = grads
+        variance = grads.var(dim=1).mean(dim=1)
+        variance_std = grads.var(dim=1).std(dim=1)
+        policy_snr = (grads.mean(dim=1) ** 2 / (grads.var(dim=1) + 1e-9)).mean(dim=1)
+        ax[0].plot(range(H), variance, label=key)
+        # ax[0].fill_between(range(H), lower_bound, upper_bound, alpha=0.5)
+        ax[1].plot(range(H), policy_snr, label=key)
 
     print("Saving figure")
     ax[0].set_xlabel(r"$H$")
@@ -143,7 +155,7 @@ def main(cfg: dict):
     plt.savefig("sensitivity.pdf")
 
     # save data
-    torch.save(save_data, "gradients.pt")
+    torch.save(save_data, "gradients.pt")  # lots of data! need to compreess
 
 
 def get_grads(model):
